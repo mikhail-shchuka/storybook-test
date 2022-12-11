749b31260e75029ce5a107393fa016c8fb61a930
Mikhail Shchuka <97917366+mikhail-shchuka@users.noreply.github.com>
2022-12-11 02:12:14 +0200

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -4,14 +4,14 @@ import { Zone } from "./types/zone"
 export const receivedZones: Array<Zone> = [
   { id: "1", media: cardMedia, title: "Flood zone 1", isChecked: false },
   { id: "2", media: cardMedia, title: "Flood zone 2", isChecked: false },
-  { id: "1", media: cardMedia, title: "Flood zone 3", isChecked: false },
-  { id: "2", media: cardMedia, title: "Flood zone 4", isChecked: false },
-  { id: "1", media: cardMedia, title: "Flood zone 5", isChecked: false },
-  { id: "2", media: cardMedia, title: "Flood zone 6", isChecked: false },
-  { id: "1", media: cardMedia, title: "Flood zone 7", isChecked: false },
-  { id: "2", media: cardMedia, title: "Flood zone 8", isChecked: false },
-  { id: "1", media: cardMedia, title: "Flood zone 9", isChecked: false },
-  { id: "2", media: cardMedia, title: "Flood zone 10", isChecked: false },
-  { id: "1", media: cardMedia, title: "Flood zone 11", isChecked: false },
-  { id: "2", media: cardMedia, title: "Flood zone 12", isChecked: false }
+  { id: "3", media: cardMedia, title: "Flood zone 3", isChecked: false },
+  { id: "4", media: cardMedia, title: "Flood zone 4", isChecked: false },
+  { id: "5", media: cardMedia, title: "Flood zone 5", isChecked: false },
+  { id: "6", media: cardMedia, title: "Flood zone 6", isChecked: false },
+  { id: "7", media: cardMedia, title: "Flood zone 7", isChecked: false },
+  { id: "8", media: cardMedia, title: "Flood zone 8", isChecked: false },
+  { id: "9", media: cardMedia, title: "Flood zone 9", isChecked: false },
+  { id: "10", media: cardMedia, title: "Flood zone 10", isChecked: false },
+  { id: "11", media: cardMedia, title: "Flood zone 11", isChecked: false },
+  { id: "12", media: cardMedia, title: "Flood zone 12", isChecked: false }
 ]
