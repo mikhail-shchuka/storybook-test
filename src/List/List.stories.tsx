d26211519e78609853457fc46bc47fae7d3429b4
Mikhail Shchuka <97917366+mikhail-shchuka@users.noreply.github.com>
2022-12-11 02:11:22 +0200

    Update List.stories.tsx

--- src/List/List.stories.tsx
+++ src/List/List.stories.tsx
@@ -1,17 +1,29 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { useState } from "react"
 
 import { List } from "."
 import { receivedZones } from "../data"
+import { Zone } from "../types/zone"
 
 export default {
   title: "Components/List",
   component: List,
   args: {
     zones: receivedZones,
-    onSetZones: () => {}
+    onSetZones: () => {},
+    numberOfVisibleChildren: receivedZones.length
+  },
+  argTypes: {
+    numberOfVisibleChildren: {
+      control: { type: "number", min: 1, max: receivedZones.length, step: 1 }
+    }
   }
 } as ComponentMeta<typeof List>
 
-const Template: ComponentStory<typeof List> = (args) => <List {...args} />
+const Template: ComponentStory<typeof List> = (args) => {
+  const [zones, setZones] = useState<Array<Zone>>(receivedZones)
+
+  return <List {...args} zones={zones} onSetZones={setZones} />
+}
 
 export const Primary = Template.bind({})
